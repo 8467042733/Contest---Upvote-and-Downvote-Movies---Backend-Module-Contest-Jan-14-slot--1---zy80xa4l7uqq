54f6d5b7af3c3ab9ebef111aae0f7d7680ef75dd
8467042733 <rupeshpal25112004@gmail.com>
2024-01-14 11:29:27 +0530

    add some code

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,6 +2,8 @@ const Movie = require('../models/Movie');
 
 exports.getAllMovies = async (req, res) => {
   try {
+    const movie=await Movie.find();
+    res.status(200).json(movies);
     // TODO: Fetch all movies from the database
     // TODO: Send a JSON response with the movies
     // res.status(200).json(movies);
@@ -12,6 +14,16 @@ exports.getAllMovies = async (req, res) => {
 };
 exports.upvoteMovie = async (req, res) => {
   try {
+    const movieId=req.params.movieId;
+    const upvotedMovie=await Movie.findByIdAndUpdate(
+      movieId,
+      {$inc:{upvotes:1}},
+     {new:true}
+    );
+    res.status(200).json({
+      message:'Movie upvoted successfully',
+      upvotedMovie:upvotedMovie,
+    });
     // TODO: Retrieve the movie ID from the request parameters
     // TODO: Update the movie's upvotes using $inc and retrieve the updated movie
     // TODO: Send a success response with the upvoted movie details
@@ -24,6 +36,17 @@ exports.upvoteMovie = async (req, res) => {
 
 exports.downvoteMovie = async (req, res) => {
   try {
+    const movieId=req.params.movieId;
+    const downvotedMovie=await Movie.findByIdAndUpdate(
+      movieId,
+      {$inc:{downvotes:1}},
+     {new:true}
+    );
+    res.status(200).json({
+      message:'Movie downvoted successfully',
+      downvotedMovie:downvotedMovie,
+    });
+
     // TODO: Retrieve the movie ID from the request parameters
     // TODO: Update the movie's downvotes using $inc and retrieve the updated movie
     // TODO: Send a success response with the downvoted movie details
